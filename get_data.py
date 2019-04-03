749d3b9cfffb1ba4f070b41ef54d34db932654c1
Jonathan Havens <jhavens12@gmail.com>
2019-04-03 19:32:24 -0400

    revert

--- get_data.py
+++ get_data.py
@@ -99,11 +99,11 @@ def forecast_me_2():
         current_timestamp = datetime.datetime.now()
         for hour in boston.hourly:
             time = datetime.datetime.fromtimestamp(hour.time)
-            if calendar.day_name[time.weekday()] == 'Saturday' or calendar.day_name[time.weekday()] == 'Sunday':
-            #if hour['FCTTIME']['weekday_name'] == 'Saturday' or hour['FCTTIME']['weekday_name'] == 'Sunday':
-                am_hour = '7'
-            else:
-                am_hour = '5'
+
+            # if calendar.day_name[time.weekday()] == 'Saturday' or calendar.day_name[time.weekday()] == 'Sunday':
+            #     am_hour = '7'
+            # else:
+            am_hour = '5'
 
             if time.hour == am_hour:
                 if time < current_timestamp + datetime.timedelta(days=3): #if date is within three days
